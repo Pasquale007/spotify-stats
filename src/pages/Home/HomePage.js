3141d6e38fea01f54b0fc97de66a049f2f8bed84
Thurow <pascal.thurow@accenture.com>
2022-10-11 16:55:26 +0200

    add first idea infinitive loading

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -17,12 +17,24 @@ export default function HomePage() {
     const whyUs = ["It's Free", "No configuration necessary", "Easy to use", "Simple design", "Easy connect via your spotify account", "Access all time history from spotify direct"];
 
     useEffect(() => {
+        let scrollElement = document.getElementById(style.infinitiveScrollPlaceholder)
+        console.log(scrollElement)
+        let observer = new IntersectionObserver(element => { loadContent() })
+        observer.observe(scrollElement)
+    }, [])
+
+    useEffect(() => {
         async function recommendations() {
             let recommendations = await HelperFunctions.fetchRecommendations(visibleData);
             setRecommendations(recommendations);
         }
         recommendations();
     }, [visibleData, timeRange])
+
+    async function loadContent() {
+        let newData = await HelperFunctions.fetchRecommendations(visibleData);
+        setRecommendations(recommendations => [...recommendations, ...newData])
+    }
 
     return (
         <div className={style.root}>
@@ -52,6 +64,9 @@ export default function HomePage() {
                         <DetailedTracks key={track.id} data={track} />
                     );
                 })}
+                <div id={style.infinitiveScrollPlaceholder}>
+                    <p>asdf</p>
+                </div>
             </div>
         </div >
     );
