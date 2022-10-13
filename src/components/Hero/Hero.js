8a06da318ba6f68c853e3bde7e18d0fd3da282ee
Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
2022-10-13 21:28:34 +0200

    add hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,14 @@
-import RotatingWords from '../RotatingWords/RotatingWords';
 import style from './Hero.module.css'
 
 export default function Hero() {
-
+    const phrase1 = " Music connects.";
+    const phrase2 = " Let's connect together.";
     return (
         <div className={style.main}>
             <h1 id={style.catchingPhrase}>
-                Music connects. <br /> Let's connect together.
+                <b>{phrase1}</b>
+                <br />
+                <b>{phrase2}</b>
             </h1>
         </div>
 
